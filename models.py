ca6b4ca72dc266108a7affca43e0153e0444550e
apoorv-x12 <fifa.ajay219@gmail.com>
2026-02-21 03:13:36 +0530

    added WorflowStep model and added the foreign key relation with Workflow and also added the relationship attribute
    added WorflowStep model and added the foreign key relation with Workflow and also added the relationship attribute

--- models.py
+++ models.py
@@ -1,9 +1,20 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 from db import Base
-
 
 class Workflow(Base):
     __tablename__ = "workflows"
 
     id = Column(Integer, primary_key=True, index=True)
     status = Column(String, nullable=False)
+    steps = relationship("WorkflowStep", back_populates="workflow")
+    
+class WorkflowStep(Base):
+    __tablename__ = "workflow_steps"
+
+    id = Column(Integer, primary_key=True, index=True)
+    workflow_id = Column(Integer, ForeignKey("workflows.id"), nullable=False)
+    name = Column(String, nullable=False)
+    status = Column(String, nullable=False)
+    workflow=relationship("Workflow", back_populates="steps")
+    
