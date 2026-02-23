1b7338cea6bc6a28fe26a7f5892c2c334d9d1b56
apoorv-x12 <fifa.ajay219@gmail.com>
2026-02-24 04:49:30 +0530

    modified the models with unique contraint due to lost update problem and also added timestamps for worker plane and observability
    modified the models with unique constraint due to lost update problem and also added timestamps for worker plane and observability

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String, ForeignKey, UniqueConstraint, DateTime, func
 from sqlalchemy.orm import relationship
 from db import Base
 
@@ -7,6 +7,9 @@ class Workflow(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     status = Column(String, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+    deleted_at = Column(DateTime(timezone=True), nullable=True)
     steps = relationship("WorkflowStep", back_populates="workflow")
     
 class WorkflowStep(Base):
@@ -16,6 +19,16 @@ class WorkflowStep(Base):
     workflow_id = Column(Integer, ForeignKey("workflows.id"), nullable=False, index=True)
     name = Column(String, nullable=False)
     status = Column(String, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+    deleted_at = Column(DateTime(timezone=True), nullable=True)
     step_number = Column(Integer, nullable=False)
     workflow=relationship("Workflow", back_populates="steps")
+
+    __table_args__ = (
+        # Ensure that step_number is unique within the same workflow
+        UniqueConstraint('workflow_id', 'step_number', name='uix_workflow_step_number'),
+        # Ensure that name is unique within the same workflow
+        UniqueConstraint('workflow_id', 'name', name='uix_workflow_step_name'),
+    )
     
