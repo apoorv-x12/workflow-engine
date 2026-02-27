64fcffccd77137f65393ae9a07f435ba8befbc5d
apoorv-x12 <fifa.ajay219@gmail.com>
2026-02-27 19:25:47 +0530

    Create test_multi_worker_step_claim_race_processes.py
    added a multi worker test to check correctness using multiprocessing

---
+++ test_multi_worker_step_claim_race_processes.py
@@ -0,0 +1,21 @@
+import subprocess
+import multiprocessing
+
+PROCESSES=10
+
+def run_worker_process(worker_id):
+    print(f"Starting worker process: {worker_id}")
+    subprocess.run(["python", "worker.py"])
+
+# For testing, we can run multiple worker processes to simulate
+# write inside if __name__ == "__main__" to avoid issues with multiprocessing on Windows
+if __name__ == "__main__":
+    processes=[]
+    for i in range(PROCESSES):
+        p=multiprocessing.Process(target=run_worker_process, args=(i,))
+        processes.append(p)
+        p.start()
+
+    for p in processes:
+        p.join()
+
